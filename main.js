354e3fc655b98cb6c95924981ba92e79ea939f8c
pranavkdileep <pranavdileep10@gmail.com>
2025-07-20 18:14:42 +0530

    go

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 import ProxyChain from 'proxy-chain';
 import ngrok from '@ngrok/ngrok';
 
-const server = new ProxyChain.Server({ port: 8081 });
+const server = new ProxyChain.Server({
+  port: 8081,
+  prepareRequestFunction: ({ request }) => {
+    const url = request.url;
+    const method = request.method;
+    const headers = request.headers;
+
+    console.log(`\n[REQUEST] ${method} ${url}`);
+    console.log('Headers:', headers);
+
+    return {
+      upstreamProxyUrl: null,
+    };
+  },
+
+  verbose: true,
+});
 server.listen(async () => {
-    try {
-        const builder = new ngrok.SessionBuilder();
-        builder.authtokenFromEnv().metadata("ProxyChain UNIX socket");
-      
-        builder.connect().then((session) => {
-          session
-            .tcpEndpoint()
-            .metadata("ProxyChain listener")
-            .listen()
-            .then((listener) => {
-              console.log("Ingress established at:", listener.url().replace('tcp://', 'http://'));
-              listener.forward('http://localhost:8081')
-            });
+  try {
+    const builder = new ngrok.SessionBuilder();
+    builder.authtokenFromEnv().metadata("ProxyChain UNIX socket")
+    .handleDisconnection((addr, error) => {
+      console.log(`[ngrok] Disconnected from ${addr}: ${error}`);
+      return true;
+    })
+    .handleHeartbeat((latency) => {
+      console.log(`[ngrok] Heartbeat latency: ${latency} ms`);
+    })
+    .handleRestartCommand(() => {
+      console.log('[ngrok] Restart command received from ngrok service.');
+    })
+    .heartbeatTolerance(100);
+
+    builder.connect().then((session) => {
+      session
+        .tcpEndpoint()
+        .metadata("ProxyChain listener")
+        .listen()
+        .then((listener) => {
+          console.log("Ingress established at:", listener.url().replace('tcp://', 'http://'));
+          listener.forward('http://localhost:8081')
         });
-    } catch (error) {
-        console.error('Failed to start ngrok tunnel:', error);
-        process.exit(1);
-    }
+    });
+  } catch (error) {
+    console.error('Failed to start ngrok tunnel:', error);
+    process.exit(1);
+  }
 });
