afabab7c92a21c83e19176fe24f560f2815dffa3
pranavkdileep <pranavdileep10@gmail.com>
2025-07-20 16:25:01 +0530

    hi

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 import ProxyChain from 'proxy-chain';
-import ngrok from 'ngrok';
+import ngrok from '@ngrok/ngrok';
 
 const server = new ProxyChain.Server({ port: 8081 });
 server.listen(async () => {
     try {
-        const url = await ngrok.connect({
-            addr: 8081,
-            proto: 'tcp',
-            authtoken : process.env.NGROK_AUTHTOKEN || '<YOUR_NGROK_AUTHTOKEN>',
+        const builder = new ngrok.SessionBuilder();
+        builder.authtokenFromEnv().metadata("ProxyChain UNIX socket");
+      
+        builder.connect().then((session) => {
+          session
+            .tcpEndpoint()
+            .metadata("ProxyChain listener")
+            .listen()
+            .then((listener) => {
+              console.log("Ingress established at:", listener.url());
+              listener.forward('http://localhost:8081')
+            });
         });
-        console.log(`Tunnel URL: ${url}`);
-        console.log('Proxy server is running on port 8081');
     } catch (error) {
         console.error('Failed to start ngrok tunnel:', error);
         process.exit(1);
